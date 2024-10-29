94e0bd811220e112915dae5418bdb9153d914ab3
Lumi Xu <102558017+Lumi-works@users.noreply.github.com>
2024-10-29 14:59:33 -0500

    schedule

--- .contentlayer/generated/Doc/_index.mjs
+++ .contentlayer/generated/Doc/_index.mjs
@@ -2,11 +2,11 @@
 
 import chapter1__section1Mdx from './chapter1__section1.mdx.json' assert { type: 'json' }
 import chapter1__section2Mdx from './chapter1__section2.mdx.json' assert { type: 'json' }
-import resource__rmEsMdx from './resource__RMEs.mdx.json' assert { type: 'json' }
-import resource__setupMdx from './resource__Setup.mdx.json' assert { type: 'json' }
-import resource__workspacesMdx from './resource__Workspaces.mdx.json' assert { type: 'json' }
 import mps__musicalTreesMdx from './mps__MusicalTrees.mdx.json' assert { type: 'json' }
 import mps__seamCarverMdx from './mps__SeamCarver.mdx.json' assert { type: 'json' }
 import mps__wordGuessMdx from './mps__WordGuess.mdx.json' assert { type: 'json' }
+import resource__rmEsMdx from './resource__RMEs.mdx.json' assert { type: 'json' }
+import resource__setupMdx from './resource__Setup.mdx.json' assert { type: 'json' }
+import resource__workspacesMdx from './resource__Workspaces.mdx.json' assert { type: 'json' }
 
-export const allDocs = [chapter1__section1Mdx, chapter1__section2Mdx, resource__rmEsMdx, resource__setupMdx, resource__workspacesMdx, mps__musicalTreesMdx, mps__seamCarverMdx, mps__wordGuessMdx]
+export const allDocs = [chapter1__section1Mdx, chapter1__section2Mdx, mps__musicalTreesMdx, mps__seamCarverMdx, mps__wordGuessMdx, resource__rmEsMdx, resource__setupMdx, resource__workspacesMdx]
